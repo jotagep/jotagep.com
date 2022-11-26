7c0d4d31a24bc8bcb7fb9b4225b2685f5032ccc1
jotagep <jotagmzn@gmail.com>
2022-11-26 13:02:54 +0100

    first commit

---
+++ src/data/presentation.ts
@@ -0,0 +1,34 @@
+type Social = {
+  label: string;
+  link: string;
+};
+
+type Presentation = {
+  mail: string;
+  title: string;
+  description: string;
+  socials: Social[];
+};
+
+const presentation: Presentation = {
+  mail: "maxencewolff.pro@gmail.com",
+  title: "Hi, I’m Maxence 👋",
+  description:
+    "Bonjour, i'm a *french frontend developer* with over *3 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
+  socials: [
+    {
+      label: "Twiiter",
+      link: "https://twitter.com/itsstormzz_",
+    },
+    {
+      label: "Bento",
+      link: "https://bento.me/m-wolff",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/MaeWolff",
+    },
+  ],
+};
+
+export default presentation;
